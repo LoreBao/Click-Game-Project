f89a3672d966a512c5b5f7518f0f6385939dafce
LoreBao <kevin.louis.1170@gmail.com>
2025-08-06 15:00:42 +0800

    NEW JSON FILE!
    NEW CLASS: SPRITEMANAGER
    
    Update BasicJerry

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ const HEIGHT=800;
 canvas.width=WIDTH;
 canvas.height=HEIGHT;
 
+let config={};
+
+(async function(){
+    const configData=await fetch(".static/config.json").then(r=>r.json());
+    config=Object.fromEntries(configData.map(function(value){
+        const img=new Image();
+        img.src="./static/"+value.img;
+        return [value.type,{...value,img}];
+    }));
+})();
+
 class BasicWeapons{
     constructor(image,score,price){
         this.image=image; //img:"image path"//
@@ -28,9 +39,27 @@ class BasicWeapons{
 }
 
 class BasicJerrys{
-    constructor(image,score){
-        this.image=image; //img:"image path"//
-        this.score=score; //score:"pts/click"//
+    constructor(config){
+        this.image=config.img; //img:"image path"//
+        this.reward=config.reward;
+        this.pos=this.randomPos();
+        this.width=config.width;
+        this.height=config.height;
+    }                                   
+
+    randomPos(){
+        let randomX=Math.floor(Math.random()*(WIDTH-this.width));
+        let randomY=Math.floor(Math.random()*(HEIGHT-this.height));
+
+        return {
+            x: randomX,
+            y: randomY,
+        }
+    }
+
+    clicked(ClickPos){
+            return (ClickPos.x>=this.pos.x&&ClickPos.x<=this.width+this.pos.x
+            &&ClickPos.y>=this.pos.y&&ClickPos.y<=this.height+this.pos.y);
     }
 
     action(){
@@ -39,6 +68,26 @@ class BasicJerrys{
 
     draw(){
         
+    }
+}
+
+class SpriteManager{
+    constructor(config){
+        this.img=config.img;
+        this.sx=config.sx;
+        this.sy=config.sy;
+        this.width=config.width;
+        this.height=config.height;
+        this.frameCount=config.frameCount;
+        this.frameDuration=config.frameDuration;
+        this.frameIndex=0;
+        this.nextframe=Date.now()+this.frameDuration;
+    }
+
+    drawSprite(){
+        if(Date.now>this.nextframe){
+            this.frameIndex+=;
+        }
     }
 }
 
@@ -57,5 +106,6 @@ class GameManager{
             this.clickArea.y=e.clientY;
         })
     }
+
 }
 
